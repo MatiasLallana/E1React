516a0e96bcd85f9dab412162a0ec2081f27a4b05
MatiasLallana <matias.lallana@outlook.es>
2024-07-08 14:55:49 -0300

    sin comentarios

--- src/assets/components/Products/CardProducts.jsx
+++ src/assets/components/Products/CardProducts.jsx
@@ -5,7 +5,7 @@ export const CardProducts = (props) => {
   const { img, model, specs, price } = props;
   return (
     <s.ProductCard>
-      <s.ProductImage src={img} alt="{products[0].img}" />
+      <s.ProductImage src={img} alt="{model}" />
       <s.ProductTitle>{model}</s.ProductTitle>
       <s.ProductText>{specs}</s.ProductText>
       <s.ProductPrice>${price}</s.ProductPrice>
